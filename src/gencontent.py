da74743a82780c3a8853572ff2047f095de5aa47
bartucansahin <bartucan.dev@gmail.com>
2024-04-10 20:14:16 +0300

    A page is now generated through using the input from /content/index.md, a markdown file to html.

--- src/gencontent.py
+++ src/gencontent.py
@@ -6,22 +6,30 @@ def extract_title(markdown):
         file_contents = md_file.read()
     splitted_on_lines = file_contents.split("\n")
     if splitted_on_lines[0].startswith("# "):
-        return splitted_on_lines[0][1:].strip()
+        return splitted_on_lines[0].strip("# ")
     raise Exception("All pages need a h1 header")
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
-    with open(from_path, "r") as from_path_file:
-        from_path_file_contents = from_path_file.read()
+    try:
+        with open(from_path, "r") as from_file:
+            markdown_content = from_file.read()
 
-    with open(template_path, "r") as template_file:
-        template_path_file_cont = template_file.read()
-    
-    html = markdown_to_HTMLNode(from_path_file_contents)
-    title = extract_title(from_path_file_contents)
+        with open(template_path, "r") as template_file:
+            template_content = template_file.read()
 
-    new_content = template_path_file_cont.replace("{{ Title }}", title).replace("{{ Content }}", html)
-    
+        html_content = markdown_to_HTMLNode(markdown_content)
+        title = extract_title(from_path)
+
+        new_content = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+
+        with open(dest_path, "w") as dest_file:
+            dest_file.write(new_content)
+
+    except FileNotFoundError:
+        raise Exception("File not found.")
+    except Exception as e:
+        raise Exception(f"Error generating page: {e}")
 
     
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from gencontent import generate_page
 
 def main():
     if os.path.exists("public"):
@@ -7,5 +8,7 @@ def main():
     elif not os.path.exists("public"):
         os.mkdir("public")
     shutil.copytree("static", "public")
+
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 main()
